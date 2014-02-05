0d084d7dd2b4baa8bc29a85bf8ba450f03cbd15e
ashramwen <ashramwen@gmail.com>
2014-02-05 11:06:39 +0800

    img hover

--- js/editor.js
+++ js/editor.js
@@ -1,15 +1,21 @@
 $(function() {
-	$('.slideshow-container').dacSlideshow({
+	$('[data-type="BannerArea"]').dacSlideshow({
 		btnPrev : '.slideshow-prev',
 		btnNext : '.slideshow-next',
 		btnPause : '.pauseButton'
 	});
 
+	$('[data-type="AdArea"]').hover(function() {
+		$(this).find('[data-type="AdImage"]:first').css("visibility", "visible");
+	}, function() {
+		$(this).find('[data-type="AdImage"]:first').css("visibility", "hidden");
+	});
+
 	var editors = $('#strContent').PSEditor({
 		getImgUrl : 'testjson2'
 	});
-	
-	$("#getdata").click(function(){
+
+	$("#getdata").click(function() {
 		console.log(JSON.stringify(editors.Get(0)));
 		console.log(JSON.stringify(editors.GetAll()));
 	});
